5796f754b49c4cf7911a674341ecab15cb73ac5a
ginpei <ginpei@gmail.com>
2016-01-09 16:08:40 -0800

    move the codes of LiveReload from inside HTML to a JS file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 (function(window, document) {
+	// Example
 	let swooshTable = new SwooshTable('.js-table');
 	swooshTable.on('click', function(event, row, elButton) {
 		if (elButton.classList.contains('rowTools-item-delete')) {
@@ -8,4 +9,11 @@
 			});
 		}
 	});
+
+	// LiveReload
+	if (location.search === '?live') {
+		var elScript = document.createElement('script');
+		elScript.src = '//' + location.hostname + ':35729/livereload.js';
+		document.body.appendChild(elScript);
+	}
 })(window, document);
