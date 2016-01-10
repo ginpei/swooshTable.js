9636d7bcc47264ca3073d29be604139ddc95fb4b
ginpei <ginpei@gmail.com>
2016-01-09 16:08:10 -0800

    move setting up codes from inside HTML to a JS file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 (function(window, document) {
+	let swooshTable = new SwooshTable('.js-table');
+	swooshTable.on('click', function(event, row, elButton) {
+		if (elButton.classList.contains('rowTools-item-delete')) {
+			row.restore();
+			row.$el.slideUp(function() {
+				swooshTable.removeRow(row);
+			});
+		}
+	});
 })(window, document);
